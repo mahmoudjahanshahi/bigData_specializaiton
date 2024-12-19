e27adc76fe8b98062c07f808ac64cc20153a9513
mahmoudjahanshahi <mahmoud.jahanshahi@gmail.com>
2024-12-19 15:09:58 -0500

    finished course 1

--- course1/script.sh
+++ course1/script.sh
@@ -8,12 +8,25 @@ docker-compose up -d
 docker cp ./mapred-site.xml hadoop-namenode-1:/opt/hadoop/etc/hadoop/mapred-site.xml 
 docker cp ./words.txt hadoop-namenode-1:/opt/hadoop/words.txt
 
-# run wordcount on hadoop
+# get into container environment and copy word.txt
 docker exec -it hadoop-namenode-1 /bin/bash
+hdfs dfs -mkdir -p /user/hadoop
 hdfs dfs -copyFromLocal words.txt
+
+# run wordcount on hadoop
 yarn jar share/hadoop/mapreduce/hadoop-mapreduce-examples-3.3.6.jar wordcount words.txt out
 hdfs dfs -copyToLocal out/part-r-00000 local.txt
-exit
+# exit
 
 # retrieve results
-docker cp hadoop-namenode-1:/opt/hadoop/local.txt ../local.txt
+docker cp hadoop-namenode-1:/opt/hadoop/local.txt ../wordcount_results.txt
+
+# quiz
+docker cp ./alice.txt hadoop-namenode-1:/opt/hadoop/alice.txt 
+docker exec -it hadoop-namenode-1 /bin/bash
+hdfs dfs -copyFromLocal alice.txt
+yarn jar share/hadoop/mapreduce/hadoop-mapreduce-examples-3.3.6.jar wordcount alice.txt quiz
+hdfs dfs -copyToLocal quiz/part-r-00000 result.txt
+yarn jar share/hadoop/mapreduce/hadoop-mapreduce-examples-3.3.6.jar wordmedian alice.txt quiz2
+# exit
+docker cp hadoop-namenode-1:/opt/hadoop/result.txt ../quiz_results.txt
